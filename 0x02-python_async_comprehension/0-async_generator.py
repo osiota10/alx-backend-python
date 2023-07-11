dcc2d8e980ea0013896d35f23c683fe5e14dbc36
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2023-07-11 13:50:36 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,3 +8,7 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
+
+async def consume_async_generator():
+    async for value in async_generator():
+        print(value)
